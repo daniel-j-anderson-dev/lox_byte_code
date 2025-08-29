3883808367aa39b9fe00f6feb1ed5e96be39f8b7
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-08-29 16:01:50 -0600

    updated opcode

--- crates/byte_code_compiler/src/opcode.rs
+++ crates/byte_code_compiler/src/opcode.rs
@@ -1,5 +1,17 @@
 /// A single instruction to the VM
-pub enum OpCode {
+#[derive(Debug, Clone, Copy)]
+pub enum Opcode {
     /// Return from the current function
     Return,
+}
+impl Opcode {
+    pub const fn new(byte: u8) -> Option<Self> {
+        match byte {
+            0 => Some(Self::Return),
+            _ => None,
+        }
+    }
+    pub const fn as_byte(&self) -> u8 {
+        *self as _
+    }
 }
