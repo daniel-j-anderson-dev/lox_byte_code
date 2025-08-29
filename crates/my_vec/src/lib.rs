07cdfd22710aff45b0a718c88610271d232fd00f
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-08-29 12:32:53 -0600

    split the raw array into a module

--- crates/my_vec/src/lib.rs
+++ crates/my_vec/src/lib.rs
@@ -1,112 +1,15 @@
 pub mod error;
+pub mod raw;
 
 use std::{
-    alloc::{Layout, LayoutError, alloc, dealloc, handle_alloc_error, realloc},
-    mem::size_of,
     ops::{Deref, DerefMut},
-    ptr::{self, NonNull},
-    usize,
+    ptr,
 };
 
-use error::GrowError;
-
-use crate::error::InsertError;
-
-pub const MAX_ALLOCATION_SIZE: usize = isize::MAX as _;
-
-pub struct RawDynamicSizeArray<T> {
-    elements: NonNull<T>,
-    capacity: usize,
-}
-
-unsafe impl<T: Send> Send for RawDynamicSizeArray<T> {}
-unsafe impl<T: Sync> Sync for RawDynamicSizeArray<T> {}
-
-// constructors
-impl<T> RawDynamicSizeArray<T> {
-    const ELEMENT_SIZE: usize = size_of::<T>();
-    const NEW_CAPACITY: usize = if Self::ELEMENT_SIZE == 0 {
-        usize::MAX
-    } else {
-        usize::MIN
-    };
-
-    pub const fn new() -> Self {
-        Self {
-            elements: NonNull::dangling(),
-            capacity: Self::NEW_CAPACITY,
-        }
-    }
-}
-
-// accessors
-impl<T> RawDynamicSizeArray<T> {
-    const fn layout(&self) -> Result<Layout, LayoutError> {
-        Layout::array::<T>(self.capacity)
-    }
-}
-
-// mutators
-impl<T> RawDynamicSizeArray<T> {
-    /// Extend capacity by doubling or adding 1 at first call.
-    pub fn grow(&mut self) -> Result<(), GrowError> {
-        // [3]
-        let (new_layout, new_pointer) = if self.capacity == 0 {
-            self.capacity = 1;
-            let new_layout = self.layout()?;
-            // SAFETY: new_capacity == 1
-            let new_pointer = unsafe { alloc(new_layout) }; // [1]
-
-            (new_layout, new_pointer)
-        } else {
-            let old_layout = self.layout()?;
-            let old_pointer = self.elements.as_ptr() as _;
-
-            self.capacity *= 2; // [4]
-            let new_layout = self
-                .layout()
-                .map_err(GrowError::Layout)
-                .and_then(|new_capacity| {
-                    //[5]
-                    if new_capacity.size() <= MAX_ALLOCATION_SIZE {
-                        Ok(new_capacity)
-                    } else {
-                        Err(GrowError::AllocationTooLarge)
-                    }
-                })?;
-
-            // SAFETY:
-            // `old_pointer` was allocated with [alloc::alloc] using the same global allocator
-            // `old_layout` was use to allocate and is therefore the same as the size used to allocate. see [1], [2]
-            // `new_layout.size()` is unsigned and not 0. see [3], [4]
-            // `new_layout.size()` <= [isize::MAX]. see [5]
-            let new_pointer = unsafe { realloc(old_pointer, old_layout, new_layout.size()) }; // [2]
-
-            (new_layout, new_pointer)
-        };
-
-        self.elements =
-            NonNull::new(new_pointer as _).ok_or(GrowError::AllocationFail(new_layout))?;
-
-        Ok(())
-    }
-}
-
-impl<T> Drop for RawDynamicSizeArray<T> {
-    fn drop(&mut self) {
-        if self.capacity != 0 && Self::ELEMENT_SIZE != 0 {
-            if let Ok(layout) = self.layout() {
-                unsafe {
-                    // SAFETY:
-                    // `self.elements` was allocated by the global allocator so can be deallocated by the global allocator.
-                    // `layout` is the same use to deallocate because is exactly the same [Layout] that was used for that allocation, because
-                    //  we always compute it with `Layout::array::<T>(self.capacity)`.
-                    dealloc(self.elements.as_ptr() as _, layout);
-                }
-            }
-        }
-    }
-}
+use crate::{
+    error::{GrowError, InsertError},
+    raw::RawDynamicSizeArray,
+};
 
 pub struct DynamicSizeArray<T> {
     buffer: RawDynamicSizeArray<T>,
